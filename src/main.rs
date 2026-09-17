98a0904cfcf61a14db8d444fb0474ae02bfda0f4
ggajda <g.gajda1976@gmail.com>
2026-09-17 09:49:17 +0200

    Ver. 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 mod watch_fn;
-use std::{fs::create_dir_all, path::Path};
+use anyhow::{Ok, Result};
+use std::path::Path;
 use watch_fn::watch_callback;
 use watch_folder_lib::run_watch;
 
-fn main() {
-    let src = "src_dir";
-    let dst = "dst_dir";
-
-    create_dir_all(src).expect("Failed to create source folder");
-    create_dir_all(dst).expect("Failed to create destination folder");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let src = "dir_src";
+    let dst = "dir_dst";
 
     let src_path = Path::new(src);
     let dst_path = Path::new(dst);
 
     run_watch(src_path, dst_path, watch_callback).await?;
+
+    Ok(())
 }
--- src/watch_fn.rs
+++ src/watch_fn.rs
@@ -1,9 +1,29 @@
 use anyhow::Result;
 use notify::Event;
-use std::path::Path;
+use std::fs::copy;
+use std::path::PathBuf;
+use tokio::time::{Duration, sleep};
 
-pub async fn watch_callback(dst_path: &Path, event: Event) -> Result<()> {
-    println!("Copied!");
+pub async fn watch_callback(dst_path: PathBuf, event: Event) -> Result<()> {
+    if !event.kind.is_create() {
+        return Ok(());
+    }
+
+    for path in event.paths {
+        let Some(file_name) = path.file_name() else {
+            continue;
+        };
+
+        let destination = dst_path.join(file_name);
+
+        sleep(Duration::from_millis(500)).await;
+
+        println!("Start to copy: {:?} -> {:?}", path, destination);
+
+        copy(&path, &destination)?;
+
+        println!("Copied {:?} -> {:?}", path, destination);
+    }
 
     Ok(())
 }
